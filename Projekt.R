6dc2062d2f1eb11d540a1d91d8833e338e243d8c
Mateusz Pałczyński <mateusz.palczynski1@gmail.com>
2025-06-19 20:39:50 +0200

    asd

---
+++ Projekt.R
@@ -0,0 +1,5 @@
+install.packages("sf")  # jeśli nie masz jeszcze pakietu; wystarczy zainstalować raz i odtwarzać z biblioteki
+library(sf)     # to trzeba zawsze ładować
+install.packages("spdep")
+library(spdep)
+library(dplyr)
