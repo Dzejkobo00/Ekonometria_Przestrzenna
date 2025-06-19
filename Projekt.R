6f4335491eec544023504103dffcb26027a3359b
Mateusz Pałczyński <mateusz.palczynski1@gmail.com>
2025-06-19 20:55:16 +0200

    shp

--- Projekt.R
+++ Projekt.R
@@ -3,3 +3,9 @@ library(sf)     # to trzeba zawsze ładować
 install.packages("spdep")
 library(spdep)
 library(dplyr)
+
+setwd ("C:/Users/mateu/Desktop/STUDIA MAGISTERSKIE/2 semestr/ekonometria przestrzenna/projekt/Ekonometria_Przestrzenna/wojewodztwa.shp")
+
+dane <- st_read ("wojewodztwa.shp")
+
+print (dane)
